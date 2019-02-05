7b6aba490ae821f733c5a42a778bcfb11723f68d
SlickJohnson <liwa.johnson@gmail.com>
2019-02-04 16:27:24 -0800

    Implement s3 file uploading

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,21 +2,22 @@ if (document.querySelector("#new-pet")) {
   document.querySelector("#new-pet").addEventListener("submit", e => {
     e.preventDefault();
 
-    let pet = {};
-    const inputs = document.querySelectorAll(".form-control");
-    for (const input of inputs) {
-      pet[input.name] = input.value;
-    }
+    var form = document.getElementById("new-pet");
+    var pet = new FormData(form);
 
     axios
-      .post("/pets", pet)
+      .post("/pets", pet, {
+        headers: {
+          "Content-Type": "multipart/form-data;"
+        }
+      })
       .then(function(response) {
         window.location.replace(`/pets/${response.data.pet_id}`);
       })
       .catch(function(error) {
         const alert = document.getElementById("alert");
         alert.classList.add("alert-warning");
-        alert.textContent = "Oooooooops, s0me2ing wnet wroung. Plz tr1 agunm.";
+        alert.textContent = "Oops, something went wrong";
         alert.style.display = "block";
 
         setTimeout(() => {
